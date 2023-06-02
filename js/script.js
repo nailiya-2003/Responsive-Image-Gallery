b10d736df8d0730ef29bdf55c4e32b2752936837
Naila <naile.gashimova@gmail.com>
2023-06-03 00:12:27 +0400

    edit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 let swiperCards = new Swiper(".gallery-cards", {
     loop: true,
-    loopedSlides: 5
-  });
+    loopedSlides: 5,
+    cssMode: true,
+    effect: 'fade',
 
-  var swiper2 = new Swiper(".gallery-thumbs", {
+
+  });
+  let swiperThumbs = new Swiper(".gallery__thumbs", {
     loop: true,
     loopedSlides: 5,
-    slidersPerView:3,
-    centeredSlides:true,
-    slideToClickedSlide:true,
+    slidesPerView: 3,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
   });
+  swiperThumbs.controller.control = swiperCards
